de40b8de50716c0b62a2538c6ec05ded1bd1591e
frekkanzer2 <abatefrancesco98@gmail.com>
2019-10-01 14:48:49 +0200

    Update of the CustomAdapter

--- TestList_v2/app/src/main/java/imt/exercise/customAdapters/CustomAdapter.java
+++ TestList_v2/app/src/main/java/imt/exercise/customAdapters/CustomAdapter.java
@@ -1,7 +1,14 @@
 package imt.exercise.customAdapters;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import imt.exercise.testlist_v2.R;
 
@@ -27,25 +34,21 @@ public class CustomAdapter extends ArrayAdapter {
         this.subnamesArray = subnamesArray;
     }
 
-
-
-    //?????
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.listview_row, null,true);
-
-        //this code gets references to objects in the listview_row.xml file
-        TextView nameTextField = (TextView) rowView.findViewById(R.id.nameTextViewID);
-        TextView infoTextField = (TextView) rowView.findViewById(R.id.infoTextViewID);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView1ID);
-
-        //this code sets the values of the objects to values from the arrays
-        nameTextField.setText(nameArray[position]);
-        infoTextField.setText(infoArray[position]);
-        imageView.setImageResource(imageIDarray[position]);
-
-        return rowView;
-
-    };
+    public View getView(int position, View view, ViewGroup group){
+        //creating a LayoutInflater to generate the view
+        LayoutInflater injector = context.getLayoutInflater();
+        //creating a single row by using the inflater
+        View singleRow = injector.inflate(R.layout.complex_item, null, true);
+        //referencing all objects
+            //getting views
+        TextView textViewName = singleRow.findViewById(R.id.itemName);
+        TextView textViewSubname = singleRow.findViewById(R.id.itemSubname);
+        ImageView imageViewImg = singleRow.findViewById(R.id.itemImage);
+        //now we have to set values to the views generated
+        textViewName.setText(this.namesArray[position]);
+        textViewSubname.setText(this.subnamesArray[position]);
+        imageViewImg.setImageResource(this.imagesIDarray[position]);
+        return singleRow;
+    }
 
 }
