6817e56d0d632e47b49324da798dd22797de7583
frekkanzer2 <abatefrancesco98@gmail.com>
2019-10-01 17:40:52 +0200

    Finished TestList v2
    Finished the custom adapter
    Added the adapter to the list view

--- TestList_v2/app/src/main/java/imt/exercise/testlist_v2/MainActivity.java
+++ TestList_v2/app/src/main/java/imt/exercise/testlist_v2/MainActivity.java
@@ -3,6 +3,9 @@ package imt.exercise.testlist_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import imt.exercise.customAdapters.CustomAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] names = {
+                "Alessandro Agnello", "Daniele Sorrentino", "Davide Alfieri",
+                "Lorenzo Nunziante", "Mattia Spagnuolo"
+        };
+
+        String[] phones = {
+                "3928769139", "3408205245", "3401713930", "3458342778", "3462174578"
+        };
+
+        Integer[] images = {
+                R.drawable.img_contact, R.drawable.img_contact, R.drawable.img_contact,
+                R.drawable.img_contact, R.drawable.img_contact
+        };
+
+        CustomAdapter myAdapter = new CustomAdapter(this, images, names, phones);
+        ListView myView = findViewById(R.id.myPersonalizedList);
+        myView.setAdapter(myAdapter);
+
     }
 }
