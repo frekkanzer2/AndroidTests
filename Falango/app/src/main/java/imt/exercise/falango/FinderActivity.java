47e1d37deb9b9c5277051a2c4f6aebceb54b8616
frekkanzer2 <abatefrancesco98@gmail.com>
2019-10-10 13:31:12 +0200

    Added Falango Filter
    Added a new activity, Falango Filter

--- Falango/app/src/main/java/imt/exercise/falango/FinderActivity.java
+++ Falango/app/src/main/java/imt/exercise/falango/FinderActivity.java
@@ -1,15 +1,80 @@
 package imt.exercise.falango;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FinderActivity extends AppCompatActivity {
 
+    private Button btnPizzeria = null;
+    private Button btnBar = null;
+    private Button btnPub = null;
+    private Button btnSushi = null;
+    private Button btnClick = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finder);
+        getIds();
+        setListeners();
+    }
+
+    public void getIds(){
+        btnPizzeria = findViewById(R.id.selection_pizzeria);
+        btnPub = findViewById(R.id.selection_pub);
+        btnBar = findViewById(R.id.selection_bar);
+        btnSushi = findViewById(R.id.selection_sushi);
+        btnClick = findViewById(R.id.btnBack);
+    }
+
+    public void setListeners(){
+        btnPizzeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=Pizzeria");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+        btnPub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=Pub");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });btnBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=Bar");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+        btnSushi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=Sushi");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
 }
